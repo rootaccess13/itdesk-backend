416f039ca9ce100e7acc0665096d0f98141f4bda
rootaccess13 <mavericktena@gmail.com>
2025-01-28 19:30:51 +0800

    config

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const corsOptions = {
-  origin: 'https://itdesk-frontend.vercel.app',
+  origin: '*',
   methods: 'GET, POST, PUT, DELETE',
   allowedHeaders: 'Content-Type, Authorization',
 };
