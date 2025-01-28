87022173091090f08ae57db4cebc8ff443365276
rootaccess13 <mavericktena@gmail.com>
2025-01-28 19:11:39 +0800

    Config

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb://localhost:27017/',
+    mongoURI: 'mongodb+srv://larkclient:21void@larkclient13@itdesk.ooi6x.mongodb.net/?retryWrites=true&w=majority&appName=ITDesk',
     secretOrKey: 'secret',
     googleClientID: '36468434283-pj5p6ev61uasg63djvd4bv85ho4inm1r.apps.googleusercontent.com',
     googleClientSecret: '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'
