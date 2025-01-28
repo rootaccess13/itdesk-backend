42edf1f9c1993a766baf3d4acf11dad39f39a4cf
rootaccess13 <mavericktena@gmail.com>
2025-01-28 20:34:47 +0800

    Test

--- app.js
+++ app.js
@@ -24,26 +24,18 @@ const app = express();
 // Middleware setup
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);  // Allow cookies in CORS requests
-  res.setHeader('Access-Control-Allow-Origin', 'https://itdesk-frontend.vercel.app'); // Allow your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT'); // Allowed HTTP methods
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'); // Allowed headers
-
-  // If the method is OPTIONS, immediately respond with 200 status
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  
-  // Pass to the actual handler (controller function)
-  return await fn(req, res);
+const corsOptions = {
+  origin: 'https://itdesk-frontend.vercel.app/',  // Your frontend URL
+  credentials: true,  // Allow cookies (JWT tokens) to be sent in cross-origin requests
+  methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'], // Include 'Authorization' if sending JWT tokens
+  exposedHeaders: ['Content-Type', 'Authorization'] // Expose 'Authorization' if needed in the response
 };
 
-app.use(cors(allowCors));
+app.use(cors(corsOptions));
 
 // Handle OPTIONS preflight requests
-app.options('*', cors(allowCors));
+app.options('*', cors(corsOptions));
 
 
 // MongoDB setup
